a96e912b978acd2d5b49b23312b8d6abfc69a1cc
France Wang <Higanbana@users.noreply.github.com>
2019-09-19 17:38:15 +0200

    Change button text

--- src/components/CtaBlock.js
+++ src/components/CtaBlock.js
@@ -38,7 +38,7 @@ export default class CtaBlock extends React.Component {
                       </p>
                       <input type="hidden" name="form-name" value="contactForm" />
                       <p className="form-row form-submit">
-                        <button type="submit" className="button white large">Je m\'inscris</button>
+                        <button type="submit" className="button white large">Participer</button>
                       </p>
                     </form>
                   </div>
