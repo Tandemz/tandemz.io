a25d7bfb5885605b6a4cb3712ff72615eaf1e0c3
France Wang <Higanbana@users.noreply.github.com>
2019-09-19 16:45:25 +0200

    Adding form to CTA block

--- src/components/CtaBlock.js
+++ src/components/CtaBlock.js
@@ -17,14 +17,31 @@ export default class CtaBlock extends React.Component {
                     </p>
                     }
                   </div>
-                  {_.get(this.props, 'section.actions') && 
-                  <div className="cell block-buttons">
-                    {_.map(_.get(this.props, 'section.actions'), (action, action_idx) => (
-                    <Link key={action_idx} to={safePrefix(_.get(action, 'url'))} className="button white large">{_.get(action, 'label')}</Link>
-                    ))}
+                  <div className="post-content">
+                    {htmlToReact(_.get(this.props, 'pageContext.html'))}
+                    <form name="contactForm" method="POST" netlifyHoneypot="bot-field" data-netlify="true" id="contact-form"
+                      className="contact-form">
+                      <p className="screen-reader-text">
+                        <label>Don't fill this out if you're human: <input name="bot-field" /></label>
+                      </p>
+                      <p className="form-row">
+                        <label className="form-label">Nom</label>
+                        <input type="text" name="surname" className="form-input"/>
+                      </p>
+                      <p className="form-row">
+                        <label className="form-label">Prénom</label>
+                        <input type="text" name="name" className="form-input"/>
+                      </p>
+                      <p className="form-row">
+                        <label className="form-label">Email address</label>
+                        <input type="email" name="email" className="form-input"/>
+                      </p>
+                      <input type="hidden" name="form-name" value="contactForm" />
+                      <p className="form-row form-submit">
+                        <button type="submit" className="button">Send Message</button>
+                      </p>
+                    </form>
                   </div>
-                  }
-                </div>
               </div>
             </section>
         );
