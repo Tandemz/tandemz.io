414a26ef3edef65997bc8664bc48f04f14fce95a
France Wang <Higanbana@users.noreply.github.com>
2019-09-20 16:17:42 +0200

    Remove form in contact page

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -21,28 +21,6 @@ export default class Contact extends React.Component {
                   }
                   <div className="post-content">
                     {htmlToReact(_.get(this.props, 'pageContext.html'))}
-                    <form name="contactForm" method="POST" netlifyHoneypot="bot-field" data-netlify="true" id="contact-form"
-                      className="contact-form">
-                      <p className="screen-reader-text">
-                        <label>Don't fill this out if you're human: <input name="bot-field" /></label>
-                      </p>
-                      <p className="form-row">
-                        <label className="form-label">Nom</label>
-                        <input type="text" name="surname" className="form-input"/>
-                      </p>
-                      <p className="form-row">
-                        <label className="form-label">Prénom</label>
-                        <input type="text" name="name" className="form-input"/>
-                      </p>
-                      <p className="form-row">
-                        <label className="form-label">Email address</label>
-                        <input type="email" name="email" className="form-input"/>
-                      </p>
-                      <input type="hidden" name="form-name" value="contactForm" />
-                      <p className="form-row form-submit">
-                        <button type="submit" className="button">Send Message</button>
-                      </p>
-                    </form>
                   </div>
                 </article>
               </div>
