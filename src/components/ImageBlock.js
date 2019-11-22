a446aca4482d0c9b329c6e05aa146785d01bc1e5
France Wang <Higanbana@users.noreply.github.com>
2019-11-22 15:26:46 +0100

    Make homepage image under hero overlap with hero block + pretify code

--- src/components/ImageBlock.js
+++ src/components/ImageBlock.js
@@ -8,25 +8,12 @@ export default class ImageBlock extends React.Component {
         return (
             <section id={_.get(this.props, 'section.section_id')} className={'block text-block bg-' + _.get(this.props, 'section.bg') + ' outer'}>
               <div className="inner">
-                <div className="grid">
+                <div>
                   {_.get(this.props, 'section.image') && 
-                  <div className="cell block-preview">
+                  <div className="block-overlap">
                     <img src={safePrefix(_.get(this.props, 'section.image'))} alt={_.get(this.props, 'section.title')} />
                   </div>
                   }
-                  <div className="cell block-content">
-                    <h2 className="block-title underline">{_.get(this.props, 'section.title')}</h2>
-                    <div className="block-copy">
-                      {markdownify(_.get(this.props, 'section.content'))}
-                    </div>
-                    {_.get(this.props, 'section.actions') && 
-                    <p className="block-buttons">
-                      {_.map(_.get(this.props, 'section.actions'), (action, action_idx) => (
-                      <Link key={action_idx} to={safePrefix(_.get(action, 'url'))} className="button secondary">{_.get(action, 'label')}</Link>
-                      ))}
-                    </p>
-                    }
-                  </div>
                 </div>
               </div>
             </section>
