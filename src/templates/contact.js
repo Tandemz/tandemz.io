1db7acb3f30fc23e4215cfe58413422f0f1d934e
France Wang <Higanbana@users.noreply.github.com>
2019-11-25 14:28:38 +0100

    Add contact form

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -1,31 +1,72 @@
 import React from 'react';
 import _ from 'lodash';
 
-import {Layout} from '../components/index';
-import {htmlToReact, safePrefix} from '../utils';
+import { Layout } from '../components/index';
+import { htmlToReact, safePrefix } from '../utils';
 
 export default class Contact extends React.Component {
-    render() {
-        return (
-            <Layout {...this.props}>
-            <div className="outer">
-              <div className="inner-medium">
-                <article className="post page post-full">
-                  <header className="post-header">
-                    <h1 className="post-title">{_.get(this.props, 'pageContext.frontmatter.title')}</h1>
-                  </header>
-                  {_.get(this.props, 'pageContext.frontmatter.subtitle') && 
-                  <div className="post-subtitle">
-                    {htmlToReact(_.get(this.props, 'pageContext.frontmatter.subtitle'))}
-                  </div>
-                  }
-                  <div className="post-content">
-                    {htmlToReact(_.get(this.props, 'pageContext.html'))}
-                  </div>
-                </article>
+  render() {
+    return (
+      <Layout {...this.props}>
+        <div className="outer">
+          <div className="inner-medium">
+            <article className="post page post-full">
+              <header className="post-header">
+                <h1 className="post-title">
+                  {_.get(this.props, 'pageContext.frontmatter.title')}
+                </h1>
+              </header>
+              {_.get(this.props, 'pageContext.frontmatter.subtitle') && (
+                <div className="post-subtitle">
+                  {htmlToReact(
+                    _.get(this.props, 'pageContext.frontmatter.subtitle'),
+                  )}
+                </div>
+              )}
+              <div className="post-content">
+                {htmlToReact(_.get(this.props, 'pageContext.html'))}
+                <form
+                  name="contactForm"
+                  method="POST"
+                  netlifyHoneypot="bot-field"
+                  data-netlify="true"
+                  id="contact-form"
+                  className="contact-form"
+                >
+                  <p className="screen-reader-text">
+                    <label>
+                      Don't fill this out if you're human:{' '}
+                      <input name="bot-field" />
+                    </label>
+                  </p>
+                  <p className="form-row">
+                    <label className="form-label">Nom</label>
+                    <input type="text" name="surname" className="form-input" />
+                  </p>
+                  <p className="form-row">
+                    <label className="form-label">Prénom</label>
+                    <input type="text" name="name" className="form-input" />
+                  </p>
+                  <p className="form-row">
+                    <label className="form-label">Entreprise</label>
+                    <input type="text" name="company" className="form-input" />
+                  </p>
+                  <p className="form-row">
+                    <label className="form-label">Adresse email</label>
+                    <input type="email" name="email" className="form-input" />
+                  </p>
+                  <input type="hidden" name="form-name" value="contactForm" />
+                  <p className="form-row form-submit">
+                    <button type="submit" className="button">
+                      Send Message
+                    </button>
+                  </p>
+                </form>
               </div>
-            </div>
-            </Layout>
-        );
-    }
+            </article>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
 }
