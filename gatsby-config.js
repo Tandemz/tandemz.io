5e63059e04c9d9c9cd33e6b736c9fa1ad27116d1
France Wang <Higanbana@users.noreply.github.com>
2019-11-27 12:22:21 +0100

    Add lang attribute in html

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  pathPrefix: "/",
-  siteMetadata: require("./site-metadata.json"),
+  pathPrefix: '/',
+  siteMetadata: require('./site-metadata.json'),
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-source-data`,
@@ -9,37 +9,43 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages`
-      }
+        path: `${__dirname}/src/pages`,
+      },
     },
     {
-      resolve: `gatsby-plugin-sass`
+      resolve: 'gatsby-plugin-html-attributes',
+      options: {
+        lang: 'fr',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
     },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [`gatsby-remark-component`]
-      }
+        plugins: [`gatsby-remark-component`],
+      },
     },
     {
       resolve: `gatsby-remark-page-creator`,
-      options: {}
+      options: {},
     },
     {
       resolve: `@stackbit/gatsby-plugin-menus`,
       options: {
         sourceUrlPath: `fields.url`,
         pageContextProperty: `menus`,
-        menus: require("./src/data/menus.json")
-      }
+        menus: require('./src/data/menus.json'),
+      },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-148496948-1",
+        trackingId: 'UA-148496948-1',
         head: true,
-        anonymize: true
-      }
-    }
-  ]
+        anonymize: true,
+      },
+    },
+  ],
 };
