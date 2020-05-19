c953b0bee6db1fc9aab01d41da572e4052208f66
Nicolas Djambazian <nicolas@djambazian.fr>
2020-05-19 17:09:27 +0200

    Fix ssr

--- src/blocks/NewsLetterBlock.js
+++ src/blocks/NewsLetterBlock.js
@@ -5,18 +5,20 @@ import { SendinblueScript } from '../lib/sendinblue';
 
 class NewsLetterBlock extends React.PureComponent {
   render() {
-    window.REQUIRED_CODE_ERROR_MESSAGE = 'Veuillez choisir un code pays';
-    window.EMAIL_INVALID_MESSAGE = window.SMS_INVALID_MESSAGE =
-      'Les informations que vous avez fournies ne sont pas valides. Veuillez vérifier le format du champ et réessayer.';
-    window.REQUIRED_ERROR_MESSAGE = 'Vous devez renseigner ce champ. ';
-    window.GENERIC_INVALID_MESSAGE =
-      'Les informations que vous avez fournies ne sont pas valides. Veuillez vérifier le format du champ et réessayer.';
-    window.translation = {
-      common: {
-        selectedList: '{quantity} liste sélectionnée',
-        selectedLists: '{quantity} listes sélectionnées',
-      },
-    };
+    if (typeof window !== 'undefined' && !!window.document) {
+      window.REQUIRED_CODE_ERROR_MESSAGE = 'Veuillez choisir un code pays';
+      window.EMAIL_INVALID_MESSAGE = window.SMS_INVALID_MESSAGE =
+        'Les informations que vous avez fournies ne sont pas valides. Veuillez vérifier le format du champ et réessayer.';
+      window.REQUIRED_ERROR_MESSAGE = 'Vous devez renseigner ce champ. ';
+      window.GENERIC_INVALID_MESSAGE =
+        'Les informations que vous avez fournies ne sont pas valides. Veuillez vérifier le format du champ et réessayer.';
+      window.translation = {
+        common: {
+          selectedList: '{quantity} liste sélectionnée',
+          selectedLists: '{quantity} listes sélectionnées',
+        },
+      };
+    }
 
     return (
       <div className="sib-form">
