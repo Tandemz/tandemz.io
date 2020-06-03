cce63f21edad32d63d211555d268e759bf2367d3
Nicolas Djambazian <nicolas@djambazian.fr>
2020-06-03 14:26:49 +0200

    Fix links

--- src/utils/safePrefix.js
+++ src/utils/safePrefix.js
@@ -2,9 +2,9 @@ const { withPrefix } = require('gatsby');
 const { templateCompute } = require('./template');
 const _ = require('lodash');
 
-export default function(_url, prefix) {
+export default function (_url, prefix) {
   const url = templateCompute(_url, {
-    APP_URL: process.env.GATSBY_APP_URL,
+    APP_URL: process.env.GATSBY_APP_URL || '/',
   });
 
   if (_.startsWith(url, '#') || _.startsWith(url, 'http')) {
