35e6adbec06522b3c7fe631e3281ba587e4d3e25
Nicolas Djambazian <nicolas@djambazian.fr>
2020-07-23 14:00:34 +0200

    Async configure pixel

--- src/lib/fbPixel.js
+++ src/lib/fbPixel.js
@@ -2,7 +2,6 @@ const PIXEL_ID = '833968533701882';
 
 if (typeof window !== 'undefined' && !window.fbq) {
   const finalFbq = (window.fbq = function () {
-    console.log(arguments);
     finalFbq.callMethod
       ? finalFbq.callMethod.apply(finalFbq, arguments)
       : finalFbq.queue.push(arguments);
@@ -15,25 +14,38 @@ if (typeof window !== 'undefined' && !window.fbq) {
   finalFbq.queue = [];
 }
 
+let revoked = false;
 export const loadFbPixel = () => {
   const pixelLoaded = document.getElementById('fb-pixel-script');
 
   if (!!pixelLoaded) {
     return;
   }
-  console.log('load');
+  if (
+    (typeof navigator !== 'undefined' && navigator.doNotTrack == '1') ||
+    window.doNotTrack == '1'
+  ) {
+    return;
+  }
   const tag = document.createElement('script');
   tag.async = true;
   tag.src = 'https://connect.facebook.net/en_US/fbevents.js';
   tag.id = 'fb-pixel-script';
   document.getElementsByTagName('head')[0].appendChild(tag);
 
-  window.fbq('init', PIXEL_ID);
-  window.fbq('consent', 'grant');
-  window.fbq('track', 'PageView');
+  revoked = false;
+  setTimeout(() => {
+    window.fbq('init', PIXEL_ID);
+    window.fbq('consent', 'grant');
+    window.fbq('track', 'PageView');
+  }, 1000);
 };
 
 export const revokeConsent = () => {
+  if (revoked) {
+    return;
+  }
+  revoked = true;
   window.fbq('consent', 'revoke');
 };
 
