9eec4b97db8b0c97777a34bd499c5850ec5ad647
Shishouille <shirin.boomi@zoho.eu>
2020-10-19 16:27:42 +0200

    Design how it works block

--- src/blocks/HowItWorksBlock.js
+++ src/blocks/HowItWorksBlock.js
@@ -1,52 +1,33 @@
 import React from 'react';
 import _ from 'lodash';
 
-import { htmlToReact, safePrefix } from '../utils';
+import { safePrefix } from '../utils';
 
 export default class HowItWorksBlock extends React.Component {
   render() {
     return (
       <section
         id={_.get(this.props, 'section.section_id')}
-        className={
-          'block cell-block value-block bg-' +
-          _.get(this.props, 'section.bg') +
-          ' outer-medium'
-        }
+        className={'how-it-works block bg-' + _.get(this.props, 'section.bg')}
       >
-        {_.get(this.props, 'section.title') && (
-          <div className="block-header inner-small">
-            <h2 className="block-title">
-              {_.get(this.props, 'section.title')}
-            </h2>
-            {_.get(this.props, 'section.subtitle') && (
-              <p className="block-subtitle">
-                {htmlToReact(_.get(this.props, 'section.subtitle'))}
-              </p>
-            )}
-          </div>
-        )}
+        <h2 className="block-title">{_.get(this.props, 'section.title')}</h2>
         {_.get(this.props, 'section.steps') && (
-          <div className="inner">
-            <div className="grid">
-              {_.map(_.get(this.props, 'section.steps'), (block, block_idx) => (
-                <div key={block_idx} className="cell valueblock">
-                  <div>
-                    {_.get(block, 'illustration') && (
-                      <img
-                        className="valueblock-icon"
-                        src={safePrefix(_.get(block, 'illustration'))}
-                        alt={_.get(block, 'icon_alt')}
-                        title={_.get(block, 'icon_alt')}
-                      />
-                    )}
-                    <h3 className="valueblock-title">
-                      {_.get(block, 'caption')}
-                    </h3>
-                  </div>
+          <div className="steps-block">
+            {_.map(_.get(this.props, 'section.steps'), (block, block_idx) => (
+              <div key={block_idx} className="stepblock">
+                <div>
+                  {_.get(block, 'illustration') && (
+                    <img
+                      className="stepblock-icon"
+                      src={safePrefix(_.get(block, 'illustration'))}
+                      alt={_.get(block, 'illustation_alt')}
+                      title={_.get(block, 'illustation_alt')}
+                    />
+                  )}
+                  <p className="stepblock-caption">{_.get(block, 'caption')}</p>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
         )}
       </section>
