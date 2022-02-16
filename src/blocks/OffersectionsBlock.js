826b1c91ba42cee7c086d132f868e0cd4ebfe806
Florent Pergoud <florentpergoud@gmail.com>
2022-02-16 16:49:55 +0100

    :sparkles: (pricing columns) add full header

--- src/blocks/OffersectionsBlock.js
+++ src/blocks/OffersectionsBlock.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
+import { getLocale } from '../utils';
 
 const OfferSectionsBlock = (props) => {
   const data = _.get(props, 'tabData');
@@ -7,6 +8,7 @@ const OfferSectionsBlock = (props) => {
     return null;
   }
   const offerSections = _.get(data, 'offerSections');
+  const locale = getLocale(props.pageContext);
 
   return (
     <section id={_.get(props, 'section.section_id')} className={'bg-white'}>
@@ -14,11 +16,17 @@ const OfferSectionsBlock = (props) => {
       <div className="offer-section-content">
         {_.map(offerSections, (offerSectionData, section_idx) => {
           return (
-            <div key={`offer-section-${section_idx}`}>
-              <OfferSectionBlock offerSectionData={offerSectionData} />
-              <div
-                className="offer-section-vertical-separator"
-                key={`offer-section-separator-${section_idx}`}
+            <div
+              key={`offer-section-${section_idx}`}
+              className={
+                section_idx < offerSections.length - 1
+                  ? 'offer-column-right-border'
+                  : ''
+              }
+            >
+              <OfferSectionBlock
+                offerSectionData={offerSectionData}
+                locale={locale}
               />
             </div>
           );
@@ -37,15 +45,23 @@ const colorMatcher = {
   Black: 'blackTitle',
 };
 
-const OfferSectionBlock = ({ offerSectionData }) => {
+const OfferSectionBlock = ({ offerSectionData, locale }) => {
   const { title, color, price, offerdescription, offerdetails } =
     offerSectionData;
 
   const currentColor = colorMatcher[color];
+  const offerPriceLegend =
+    locale === 'fr' ? 'par participant' : 'per participant';
   return (
     <div className="offer-column">
       <div className="offer-column-header">
         <h4 className={`offer-title ${currentColor}`}>{title}</h4>
+        <div className="offer-price-container">
+          <span className="offer-price">{price}</span>
+          <span className="offer-price-curency">€</span>
+        </div>
+        <span className="offer-price-legend">{offerPriceLegend}</span>
+        <span className="offer-price-description">{offerdescription}</span>
       </div>
       <div className="offer-column-body"></div>
     </div>
