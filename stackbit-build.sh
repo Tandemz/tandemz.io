ece136f972b8e1be0081336e50547e224a62b874
Nicolas Djambazian <nicolas@djambazian.fr>
2022-11-16 11:20:38 +0100

    remove stackbit pull

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -4,10 +4,5 @@ set -e
 set -o pipefail
 set -v
 
-if [[ -z "${STACKBIT_API_KEY}" ]]; then
-    echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
-else
-    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5d833248eee8550011997b7e
-fi
 gatsby build
 cp _redirects ./public/
